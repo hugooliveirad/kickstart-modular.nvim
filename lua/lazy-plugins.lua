72e76442db5c11191d47f3831ddc7aa7ee3deeee
Hugo de Oliveira <hugo@triangulo.dev>
2024-03-15 19:35:25 -0300

    Setup gruvbox

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -38,8 +38,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/cmp',
 
-  require 'kickstart/plugins/tokyonight',
-
   require 'kickstart/plugins/todo-comments',
 
   require 'kickstart/plugins/mini',
