3c0e2afad5b512a98f5e79225ab86c67ee6faf84
Hugo de Oliveira <hugo@triangulo.dev>
2024-03-18 15:45:10 -0300

    Improve mini.status and add nvim-tmux-navigation

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -31,6 +31,10 @@ return {
       statusline.section_location = function()
         return '%2l:%-2v'
       end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_git = function()
+        return ''
+      end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
