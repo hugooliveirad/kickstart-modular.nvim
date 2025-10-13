5f26e46e8da9f5e43361900f2830addd99e0a30b
Hugo de Oliveira <hugo@triangulo.dev>
2025-10-13 10:40:19 -0300

    Add diffview.nvim plugin for git diff viewing
    Added diffview.nvim plugin with keymaps for viewing git diffs and file history.
    Plugin updates included in lazy-lock.json.

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,27 @@
+-- Diffview.nvim - Git diff viewer
+-- Provides a rich interface for reviewing diffs and file history
+
+return {
+  'sindrets/diffview.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  cmd = {
+    'DiffviewOpen',
+    'DiffviewClose',
+    'DiffviewToggleFiles',
+    'DiffviewFocusFiles',
+    'DiffviewRefresh',
+    'DiffviewFileHistory',
+  },
+  keys = {
+    { '<leader>dv', '<cmd>DiffviewOpen<cr>', desc = 'Open Diffview' },
+    { '<leader>dc', '<cmd>DiffviewClose<cr>', desc = 'Close Diffview' },
+    { '<leader>dh', '<cmd>DiffviewFileHistory %<cr>', desc = 'File History (current)' },
+    { '<leader>dH', '<cmd>DiffviewFileHistory<cr>', desc = 'File History (all)' },
+  },
+  opts = {
+    enhanced_diff_hl = true,
+    use_icons = true,
+  },
+}
