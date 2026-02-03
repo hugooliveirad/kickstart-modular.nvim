881d705351a0b817bcf4ca33514dfd2d38e21ca9
Hugo de Oliveira <hugo@triangulo.dev>
2026-02-03 12:39:35 -0300

    Add codediff.nvim plugin for git diff viewing with Graphite integration

---
+++ lua/custom/plugins/codediff.lua
@@ -0,0 +1,31 @@
+local utils = require 'utils'
+
+return {
+  'esmuellert/codediff.nvim',
+  dependencies = { 'MunifTanjim/nui.nvim' },
+  cmd = 'CodeDiff',
+  keys = {
+    { '<leader>dd', '<cmd>CodeDiff<cr>', desc = 'Open CodeDiff (git status)' },
+    {
+      '<leader>dm',
+      function()
+        vim.cmd('CodeDiff ' .. utils.get_parent_branch() .. '...')
+      end,
+      desc = 'Open CodeDiff (merge-base vs working tree)',
+    },
+    {
+      '<leader>dh',
+      function()
+        vim.cmd('CodeDiff history ' .. utils.get_parent_branch() .. '..HEAD')
+      end,
+      desc = 'Open CodeDiff History (parent..HEAD)',
+    },
+    { '<leader>dH', '<cmd>CodeDiff history<cr>', desc = 'Open CodeDiff History (all)' },
+  },
+  keymaps = {
+    view = {
+      next_hunk = ']g',
+      prev_hunk = '[g',
+    },
+  },
+}
