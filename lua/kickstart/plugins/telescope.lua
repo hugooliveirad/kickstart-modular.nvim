12d02adc188d9126ab9074da1cf551f1d2031153
Hugo de Oliveira <hugo@triangulo.dev>
2026-04-14 16:36:40 -0300

    Add fff.nvim for file finding and grep, replacing Telescope for those keymaps
    Move <leader>sf, <C-p>, <leader>sg, <leader>sw, and <leader>sn from
    Telescope to fff.nvim. Telescope remains for LSP pickers, buffers,
    diagnostics, and other non-file/grep functionality.

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -89,23 +89,10 @@ return {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', function()
-        builtin.find_files { hidden = true }
-      end, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<C-p>', function()
-        builtin.find_files { hidden = true }
-      end, { desc = '[S]earch [F]iles' })
+      -- <leader>sf, <C-p> and <leader>sn moved to fff.nvim (lua/custom/plugins/fff.lua)
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('v', '<leader>sw', function()
-        local old_reg = vim.fn.getreg '"'
-        local old_regtype = vim.fn.getregtype '"'
-        vim.cmd 'normal! y'
-        local search_text = vim.fn.getreg '"'
-        vim.fn.setreg('"', old_reg, old_regtype)
-        builtin.grep_string { search = search_text }
-      end, { desc = '[S]earch selected text' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      -- <leader>sw moved to fff.nvim (lua/custom/plugins/fff.lua)
+      -- <leader>sg moved to fff.nvim (lua/custom/plugins/fff.lua)
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -137,10 +124,7 @@ return {
         }
       end, { desc = '[S]earch [/] in Open Files' })
 
-      -- Shortcut for searching your neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
+      -- <leader>sn moved to fff.nvim (lua/custom/plugins/fff.lua)
 
       -- Search all files including git ignored (only ignore node_modules)
       vim.keymap.set('n', '<leader>sF', function()
